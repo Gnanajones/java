d5e22675573199aa0ee6cc3ead4ce0fc4654b052
Gnanajones <43901368+Gnanajones@users.noreply.github.com>
2018-10-06 14:12:56 +0530

    Add files via upload

---
+++ odd.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class odd
+{
+  public static void main(String args[])
+  {
+     int x;
+     Scanner in = new Scanner(System.in);
+     x = in.nextInt();
+    
+     if (x % 2 == 0)
+        System.out.println("The number is even.");
+     else if (x<=0)
+        System.out.println("Invalid");
+     else
+        System.out.println("The number is odd.");
+  }
+}
